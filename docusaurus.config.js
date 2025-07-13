c922380a86b155c6d0b30d6d97ae2615eeb289c2
mrslugmoon <jjsutton951@gmail.com>
2025-07-13 08:09:33 +0100

    Added search functionality and updated Docusaurus configuration.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -48,6 +48,8 @@ const config = {
       ({
         docs: {
           sidebarPath: './sidebars.js',
+
+          routeBasePath: '/', // Set the base path for the docs
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
@@ -102,6 +104,38 @@ const config = {
           },
         ],
       },
+      algolia: {
+      // The application ID provided by Algolia
+      appId: 'JS2ZZOVYSK',
+
+      // Public API key: it is safe to commit it
+      apiKey: '5067c0ca60c0904fbc4305d6a9c425e0',
+
+      indexName: 'Docs',
+
+      // Optional: see doc section below
+      contextualSearch: true,
+
+      // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+     // externalUrlRegex: 'external\\.com|domain\\.com',
+
+      // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+      replaceSearchResultPathname: {
+        from: '/docs/', // or as RegExp: /\/docs\//
+        to: '/',
+      },
+
+      // Optional: Algolia search parameters
+      searchParameters: {},
+
+      // Optional: path for search page that enabled by default (`false` to disable it)
+      searchPagePath: 'search',
+
+      // Optional: whether the insights feature is enabled or not on Docsearch (`false` by default)
+      insights: false,
+
+      //... other Algolia params
+    },
       footer: {
         style: 'dark',
         links: [
